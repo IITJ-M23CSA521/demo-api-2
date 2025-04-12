cc1bec82ce9fc470e4c4769524e33e6e2e7798bf
Om Solanki <om.solanki@zevo360.com>
2025-04-12 05:55:18 +0530

    change app port

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def home():
     return jsonify(message="This is demo-api-2.", status="success")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5050)
